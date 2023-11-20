c9a245b8d543932043c0cd1f6d833d2f04339b41
raunerlucas <raunerlucas@gmail.com>
2023-11-20 19:32:44 -0300

    env: implementation of class methods ServicoDAO

--- Projeto-Sistema/src/main/java/com/projeto/projetosistema/DAO/deployment/ServicoDAO.java
+++ Projeto-Sistema/src/main/java/com/projeto/projetosistema/DAO/deployment/ServicoDAO.java
@@ -5,43 +5,98 @@ import com.projeto.projetosistema.DAO.ErroDAO;
 import com.projeto.projetosistema.DAO.FabricaConexao;
 import com.projeto.projetosistema.model.Servico;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServicoDAO implements DAOInterface<Servico> {
     private Connection con;
+
     public ServicoDAO() throws ErroDAO {
         con = FabricaConexao.pegaConexao();
     }
 
     @Override
     public void inserir(Servico obj) throws ErroDAO {
+        try {
+            PreparedStatement stm = con.prepareStatement("INSERT INTO Servico(descricao,preco) values (?,?)", Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, obj.getDescricao());
+            stm.setFloat(2, obj.getPreco());
+            stm.executeUpdate();
+            ResultSet rs = stm.getGeneratedKeys();
+            if (rs.next()) {
+                obj.setId(rs.getInt(1));
+            }
+            stm.close();
+        } catch (SQLException e) {
+            throw new ErroDAO(e);
+        }
 
     }
 
     @Override
     public void deletar(Servico obj) throws ErroDAO {
-
+        deletar(obj.getId());
     }
 
     @Override
     public void deletar(int id) throws ErroDAO {
-
+        try {
+            PreparedStatement stm = con.prepareStatement("DELETE FROM Servico WHERE id = ?");
+            stm.setInt(1,id);
+            stm.executeUpdate();
+            stm.close();
+        } catch (SQLException e) {
+            throw new ErroDAO(e);
+        }
     }
 
     @Override
     public void editar(Servico ogj) throws ErroDAO {
-
+        try {
+            PreparedStatement stm = con.prepareStatement("UPDATE Sevico SET descricao = ?, preco = ? WHERE id = ?");
+            stm.setString(1,ogj.getDescricao());
+            stm.setFloat(2,ogj.getPreco());
+            stm.setInt(3,ogj.getId());
+            stm.executeUpdate();
+            stm.close();
+        } catch (SQLException e) {
+            throw new ErroDAO(e);
+        }
     }
 
     @Override
     public Servico buscar(int id) throws ErroDAO {
-        return null;
+        Servico sv = null;
+        try {
+            PreparedStatement stm = con.prepareStatement("SELECT * FROM Servico WHERE id = ?");
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                sv = new Servico(rs.getInt("id"),
+                        rs.getString("descricao"),
+                        rs.getFloat("preco"));
+            }
+        } catch (SQLException e) {
+            throw new ErroDAO(e);
+        }
+        return sv;
     }
 
     @Override
     public List<Servico> buscar() throws ErroDAO {
-        return null;
+        List<Servico> servicoList = new ArrayList<>();
+        try {
+            PreparedStatement stm = con.prepareStatement("SELECT * FROM Servico");
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()){
+             servicoList.add(new Servico(rs.getInt(1),
+                     rs.getString(2),
+                     rs.getFloat(3)));
+            }
+        } catch (SQLException e) {
+            throw new ErroDAO(e);
+        }
+        return servicoList;
     }
 
     @Override
